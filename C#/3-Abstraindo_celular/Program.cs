d558bfa63230f707b8d2c989a4232e4e874e2d72
Raphael R. Lopes <rrlopes07@gmail.com>
2023-09-09 17:41:13 -0300

    add 3 c# project

---
+++ C#/3-Abstraindo_celular/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+Nokia nokia = new Nokia("1198562415", "X211", "1862185612", 16);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+Iphone iphone = new Iphone("452136354878", "I13", "8652135486", 64);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+nokia.InstalarAplicativo("WhatsApp");
+iphone.InstalarAplicativo("Instagram");
