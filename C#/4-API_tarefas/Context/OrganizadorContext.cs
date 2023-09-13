ca25ab3d89ece3c27b92d64a17fa7deb0a44e88e
Raphael R. Lopes <rrlopes07@gmail.com>
2023-09-13 09:25:03 -0300

    Add EF API project

---
+++ C#/4-API_tarefas/Context/OrganizadorContext.cs
@@ -0,0 +1,15 @@
+using API_tarefas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_tarefas.Context
+{
+    public class OrganizadorContext : DbContext
+    {
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ C#/4-API_tarefas/Models/EnumStatusTarefa.cs
@@ -0,0 +1,8 @@
+namespace API_tarefas.Models
+{
+    public enum EnumStatusTarefa
+    {
+        Pendente,
+        Finalizado
+    }
+}
