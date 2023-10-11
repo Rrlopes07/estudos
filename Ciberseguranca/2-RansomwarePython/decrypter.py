e365b9e4c518b6a2059e834302ba62400bf7adb7
Raphael R. Lopes <rrlopes07@gmail.com>
2023-10-11 18:19:46 -0300

    add ransomware project

---
+++ Ciberseguranca/2-RansomwarePython/decrypter.py
@@ -0,0 +1,22 @@
+import os
+import pyaes
+
+## Abrir arquivo criptografado
+file_name = 'teste.txt.ransomwaretroll'
+file = open(file_name, 'rb')
+file_data = file.read()
+file.close()
+
+## Chave para descriptografar
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+decrypt_data = aes.decrypt(file_data)
+
+## Remover arquivo cruptografado
+os.remove(file_name)
+
+## Criar arquivo descriptografado
+new_file = 'teste.txt'
+new_file = open(f"{new_file}", 'wb')
+new_file.write(decrypt_data)
+new_file.close()
